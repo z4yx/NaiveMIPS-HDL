94eb29ba20f0633e201e79290be28c4da250db47
zhang <zz593141477@qq.com>
2015-11-15 14:15:07 +0800

    config CPLD as bypass

---
+++ xilinx/CPLD/top.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    13:23:04 11/15/2015 
+// Design Name: 
+// Module Name:    top 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module top(
+input wire clk,
+input wire rxd,
+output wire txd
+    );
+
+assign txd = rxd;
+
+endmodule
