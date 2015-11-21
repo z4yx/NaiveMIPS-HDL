e49098a53c8a4d176706bbd77795ca8fa819ba64
zhang <zz593141477@gmail.com>
2015-11-21 23:51:59 +0800

    keep address and data for some cycles

--- src/flash/flash_top.v
+++ src/flash/flash_top.v
@@ -27,24 +27,40 @@ output wire[31:0] bus_data_o;
 input wire bus_read;
 input wire bus_write;
 
-output wire[23:0] flash_address;
+output reg[23:0] flash_address;
 inout wire[15:0] flash_data;
-output wire flash_we_n;
+output reg flash_we_n;
 output wire flash_byte_n;
-output wire flash_oe_n;
+output reg flash_oe_n;
 output wire flash_rp_n;
 output wire[2:0] flash_ce;
-output wire flash_vpen;
+output reg flash_vpen;
 
-assign flash_ce = 3'b111;
+reg [15:0]flash_wrdata;
+
+
+always @(posedge clk_bus or negedge rst_n) begin
+  if (!rst_n) begin
+    flash_vpen <= 1'b0;
+    flash_address <= 24'b0;
+    flash_wrdata <= 16'b0;
+    flash_we_n <= 1'b1;
+    flash_oe_n <= 1'b1;
+  end
+  else if (bus_read || bus_write) begin
+    flash_vpen <= bus_write;
+    flash_address <= bus_address[23:0];
+    flash_wrdata <= bus_data_i[15:0];
+    flash_we_n <= ~bus_write;
+    flash_oe_n <= ~bus_read;
+  end
+end
+
 assign flash_rp_n = rst_n;
 assign flash_byte_n = 1'b1;
-assign flash_vpen = 1'b1;
+assign flash_ce = 3'b111;
 
-assign flash_we_n = ~bus_write;
-assign flash_oe_n = ~bus_read;
+assign flash_data = (~flash_we_n) ? flash_wrdata[15:0] : {16{1'bz}};
 assign bus_data_o = {16'b0,flash_data};
-assign flash_data = bus_write ? bus_data_i[15:0] : {16{1'bz}};
-assign flash_address = bus_address[23:0];
 
 endmodule
