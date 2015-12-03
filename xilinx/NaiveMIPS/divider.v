25a378168608550681105cf1dc7b892bbb5647fa
zhang <zz593141477@qq.com>
2015-12-03 14:36:35 +0800

    temporarily disable divider

--- xilinx/NaiveMIPS/divider.v
+++ xilinx/NaiveMIPS/divider.v
@@ -28,7 +28,11 @@ module divider(
     output [31:0] remain
     );
 
+/*
 div_gen_v3_0 x_div(
   .rfd(), .sclr(aclr), .ce(clken), .clk(clock), .dividend(numer), .quotient(quotient), .divisor(denom), .fractional(remain)
 );
+*/
+assign quotient=32'h0;
+assign remain=32'h0;
 endmodule
