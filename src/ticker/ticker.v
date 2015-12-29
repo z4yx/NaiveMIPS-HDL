77c526f8ccc1bf3ba9bb9f7d5034f3bd78c293b5
zhang <zz593141477@gmail.com>
2015-12-29 10:12:40 +0800

    prescale ticker clock

--- src/ticker/ticker.v
+++ src/ticker/ticker.v
@@ -3,7 +3,7 @@ module ticker(
   input wire            clk_bus,
   input wire            rst_n,
   
-  input wire            clk_tick,
+  input wire            clk_tick,  //10MHz clock in
   input wire            rst_tick_n,
 
   //bus
@@ -19,7 +19,7 @@ reg[31:0] ticker;
 reg[31:0] ticker_d1;
 reg[31:0] ticker_d2;
 
-
+reg[13:0] prescaler;
 
 assign bus_data_o = bus_read ? ticker_d2 : 32'd0;
 
@@ -43,8 +43,10 @@ end
 always @(posedge clk_tick or negedge rst_tick_n) begin
   if(!rst_tick_n) begin
     ticker <= 32'd0;
+    prescaler <= 14'd0;
   end else begin
-    ticker <= ticker + 1;
+    prescaler <= (prescaler < 14'd9999) ? prescaler+1'b1 : 14'd0;
+    ticker <= ticker + (prescaler == 14'd9999);  //ticker frequency is 1KHz
   end
 end
 
