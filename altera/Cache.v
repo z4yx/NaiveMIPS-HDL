88633c901439160f2820f4b65a5f8d938efb55e6
王邈 <shankerwangmiao@gmail.com>
2016-05-28 13:14:24 +0800

    add two port cache

--- altera/Cache.v
+++ altera/Cache.v
@@ -14,30 +14,158 @@ module Cache #(
 		output wire        avalon_master_read,          //             .read
 		output wire        avalon_master_write,         //             .write
 		input  wire        avalon_master_readdatavalid, //             .readdatavalid
+		
 		input  wire        rst_n,                       //        Reset.reset_n
 		input  wire        clk,                         //        Clock.clk
+		
 		input  wire [ADDR_WIDTH-1:0] avalon_slave_address,        // avalon_slave.address
 		input  wire        avalon_slave_read,           //             .read
 		input  wire        avalon_slave_write,          //             .write
 		output wire [31:0] avalon_slave_readdata,       //             .readdata
 		input  wire [31:0] avalon_slave_writedata,      //             .writedata
 		output wire        avalon_slave_waitrequest     //             .waitrequest
+		
+    input  wire [ADDR_WIDTH-1:0] avalon_rdslave_address,        // avalon_rdslave.address
+		input  wire        avalon_rdslave_read,           //             .read
+		output wire [31:0] avalon_rdslave_readdata,       //             .readdata
+		output wire        avalon_rdslave_waitrequest     //             .waitrequest
 	);
 
-	// TODO: Auto-generated HDL template
+`define INDEX_WIDTH (ADDR_WIDTH - CACHE_LINE_WIDTH - TAG_WIDTH)
+`define NUM_CACHE_LINES (2**INDEX_WIDTH)
 
-	assign avalon_master_address = 32'b00000000000000000000000000000000;
+// seperate slave addr into tag, index, offset
 
-	assign avalon_master_read = 1'b0;
+wire slave_addr_tag[ADDR_WIDTH-1: ADDR_WIDTH-TAG_WIDTH];
+wire slave_addr_idx[ADDR_WIDTH-TAG_WIDTH-1 : CACHE_LINE_WIDTH];
+wire slave_addr_off[CACHE_LINE_WIDTH-1 : 0];
+assign {slave_addr_tag, slave_addr_idx, slave_addr_off} = avalon_slave_address;
 
-	assign avalon_master_writedata = 32'b00000000000000000000000000000000;
+// separate rdslave addr into tag, index, offset
 
-	assign avalon_master_write = 1'b0;
+wire rdslave_addr_tag[ADDR_WIDTH-1: ADDR_WIDTH-TAG_WIDTH];
+wire rdslave_addr_idx[ADDR_WIDTH-TAG_WIDTH-1 : CACHE_LINE_WIDTH];
+wire rdslave_addr_off[CACHE_LINE_WIDTH-1 : 0];
+assign {rdslave_addr_tag, rdslave_addr_idx, rdslave_addr_off} = avalon_rdslave_address;
 
-	assign avalon_master_burstcount = 1'b0;
+// separate slave_waitrequest into slave_wr_waitrequest and slave_rd_waitrequest
 
-	assign avalon_slave_readdata = 32'b00000000000000000000000000000000;
+wire slave_wr_waitrequest;
+wire slave_rd_waitrequest;
+assign avalon_slave_waitrequest =  avalon_slave_read ? slave_rd_waitrequest : 
+                                  (avalon_slave_write ? slave_wr_waitrequest : 0); 
 
-	assign avalon_slave_waitrequest = 1'b0;
+// define wires for cache lines
+
+wire [ADDR_WIDTH-1 : 0]     rdAddr;
+wire [31:0]                rdDatas[`NUM_CACHE_LINES-1 : 0];
+wire                      rdVailds[`NUM_CACHE_LINES-1 : 0];
+wire                     rdDirties[`NUM_CACHE_LINES-1 : 0];
+wire                        rdHits[`NUM_CACHE_LINES-1 : 0];
+wire [TAG_WIDTH-1 : 0]      rdTags[`NUM_CACHE_LINES-1 : 0];
+
+wire [ADDR_WIDTH-1 : 0]    rd2Addr;
+wire [31:0]               rd2Datas[`NUM_CACHE_LINES-1 : 0];
+wire                     rd2Vailds[`NUM_CACHE_LINES-1 : 0];
+wire                    rd2Dirties[`NUM_CACHE_LINES-1 : 0];
+wire                       rd2Hits[`NUM_CACHE_LINES-1 : 0];
+wire [TAG_WIDTH-1 : 0]     rd2Tags[`NUM_CACHE_LINES-1 : 0];
+
+wire                        writes[`NUM_CACHE_LINES-1 : 0];
+wire [CACHE_LINE_WIDTH-1 : 0] wrOff;
+wire [TAG_WIDTH-1 : 0]        wrTag;
+wire                        wrVaild;
+wire                        wrDirty;
+wire [31:0]                  wrData;
+
+genvar cache_line_i;
+
+generate 
+
+  for (cache_line_i = 0; cache_line_i < `NUM_CACHE_LINES; cache_line_i = cache_line_i + 1) begin: cache_lines
+    cacheline #(
+      .CACHE_LINE_WIDTH (CACHE_LINE_WIDTH),
+      .TAG_WIDTH        (TAG_WIDTH),
+      .ADDR_WIDTH       (ADDR_WIDTH)
+    ) lines (
+      .clk(clk),
+      .rst_n(rst_n),
+      
+      .rdAddr              (  rdAddr),
+      .rdData              (  rdDatas[cache_line_i]),
+      .rdVaild             ( rdVailds[cache_line_i]),
+      .rdDirty             (rdDirties[cache_line_i]),
+      .rdHit               (   rdHits[cache_line_i]),
+      .rdTag               (   rdTags[cache_line_i]),
+      
+      .rd2Addr            (  rd2Addr),
+      .rd2Data            (  rd2Datas[cache_line_i]),
+      .rd2Vaild           ( rd2Vailds[cache_line_i]),
+      .rd2Dirty           (rd2Dirties[cache_line_i]),
+      .rd2Hit             (   rd2Hits[cache_line_i]),
+      .rd2Tag             (   rd2Tags[cache_line_i]),
+      
+      .write              (    writes[cache_line_i]),
+      .wrOff              (     wrOff),
+      .wrTag              (     wrTag),
+      .wrVaild            (   wrVaild),
+      .wrDirty            (   wrDirty),
+      .wrData             (    wrData)
+    );
+  end
+
+endgenerate
+
+wire cacheRewrite;
+
+wire         writesDirect[`NUM_CACHE_LINES-1 : 0];
+wire         writesRewirt[`NUM_CACHE_LINES-1 : 0];
+wire [CACHE_LINE_WIDTH-1 : 0] wrOffDirect;
+wire [CACHE_LINE_WIDTH-1 : 0] wrOffRewrit;
+wire [TAG_WIDTH-1 : 0]        wrTagDirect;
+wire [TAG_WIDTH-1 : 0]        wrTagRewrit;
+wire                        wrVaildDirect;
+wire                        wrVaildRewrit;
+wire                        wrDirtyDirect;
+wire                        wrDirtyRewrit;
+wire [31:0]                  wrDataDirect;
+wire [31:0]                  wrDataRewrit;
+
+assign writes = cacheRewrite ? writesRewrit : writesDirect
+assign wrOff = cacheRewrite ? wrOffRewrit : wrOffDirect
+assign wrTag = cacheRewrite ? wrTagRewrit : wrTagDirect
+assign wrVaild = cacheRewrite ? wrVaildRewrit : wrVaildDirect
+assign wrDirty = cacheRewrite ? wrDirtyRewrit : wrDirtyDirect
+assign wrData = cacheRewrite ? wrDataRewrit : wrDataDirect
+
+assign rdAddr   = avalon_slave_address;
+assign rd2Addr  = avalon_rdslave_address;
+
+assign avalon_slave_readdata = rdDatas[slave_addr_idx];
+assign avalon_rdslave_readdata = rd2Datas[rdslave_addr_idx];
+
+wire slaveMiss;
+wire slave2Miss;
+
+assign  slaveMiss = ! rdHits[slave_addr_idx];
+assign slave2Miss = ! rd2Hits[slave_addr_idx];
+
+assign slave_rd_waitrequest = slaveMiss;
+assign avalon_rdslave_waitrequest = slave2Miss;
+
+assign wrOffDirect = slave_addr_off;
+assign wrTagDirect = slave_addr_tag;
+assign wrVaildDirect = 1'b1;
+assign wrDirtyDirect = 1'b1;
+assign wrDataDirect = avalon_slave_writedata;
+assign slave_wr_waitrequest = slaveMiss;
+
+generate 
+  for (cache_line_i = 0; cache_line_i < `NUM_CACHE_LINES; cache_line_i = cache_line_i + 1) begin
+    assign writesDirect[i] = avalon_slave_write && rdHits[i] && i == slave_addr_idx;
+  end
+endgenerate
+
+
 
 endmodule
--- altera/cacheline.v
+++ altera/cacheline.v
@@ -13,6 +13,13 @@ module cacheline #(
   output wire                    rdHit,
   output wire [TAG_WIDTH-1 : 0]  rdTag,
   
+  input wire [ADDR_WIDTH-1 : 0]  rd2Addr,
+  output wire [31:0]             rd2Data,
+  output wire                    rd2Vaild,
+  output wire                    rd2Dirty,
+  output wire                    rd2Hit,
+  output wire [TAG_WIDTH-1 : 0]  rd2Tag,
+  
   input wire                          write,
   input wire [CACHE_LINE_WIDTH-1 : 0] wrOff,
   input wire [TAG_WIDTH-1 : 0]        wrTag,
@@ -30,14 +37,26 @@ reg [TAG_WIDTH - 1 : 0] tag;
 wire [TAG_WIDTH - 1 : 0] needTag;
 wire [CACHE_LINE_WIDTH-1 : 0] rdOff;
 
+wire [TAG_WIDTH - 1 : 0] need2Tag;
+wire [CACHE_LINE_WIDTH-1 : 0] rd2Off;
+
 assign needTag = rdAddr[ADDR_WIDTH-1 : ADDR_WIDTH-TAG_WIDTH];
 assign rdOff = rdAddr[CACHE_LINE_WIDTH-1 : 0];
+
+assign need2Tag = rd2Addr[ADDR_WIDTH-1 : ADDR_WIDTH-TAG_WIDTH];
+assign rd2Off = rd2Addr[CACHE_LINE_WIDTH-1 : 0];
 
 assign rdVaild = vaild;
 assign rdData = rdVaild ? words[ rdOff[CACHE_LINE_WIDTH-1 : 2] ] : 0;
 assign rdDirty = rdVaild ? dirty : 1'b0;
 assign rdTag = tag;
 assign rdHit = vaild && (tag == needTag);
+
+assign rd2Vaild = vaild;
+assign rd2Data = rd2Vaild ? words[ rd2Off[CACHE_LINE_WIDTH-1 : 2] ] : 0;
+assign rd2Dirty = rd2Vaild ? dirty : 1'b0;
+assign rd2Tag = tag;
+assign rd2Hit = vaild && (tag == need2Tag);
 
 always @(posedge clk, negedge rst_n) begin
 
