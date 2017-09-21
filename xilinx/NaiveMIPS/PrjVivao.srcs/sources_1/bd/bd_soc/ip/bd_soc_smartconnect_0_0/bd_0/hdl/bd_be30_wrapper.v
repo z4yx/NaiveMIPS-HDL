b799327f77e1ce7b7f1336f31879da79036904ca
zyx <zz593141477@gmail.com>
2017-09-21 13:30:46 +0800

    update block design, add video stream control

--- xilinx/NaiveMIPS/PrjVivao.srcs/sources_1/bd/bd_soc/ip/bd_soc_smartconnect_0_0/bd_0/hdl/bd_be30_wrapper.v
+++ xilinx/NaiveMIPS/PrjVivao.srcs/sources_1/bd/bd_soc/ip/bd_soc_smartconnect_0_0/bd_0/hdl/bd_be30_wrapper.v
@@ -1,7 +1,7 @@
 //Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2017.1 (lin64) Build 1846317 Fri Apr 14 18:54:47 MDT 2017
-//Date        : Wed Sep 20 15:15:56 2017
+//Date        : Thu Sep 21 12:01:54 2017
 //Host        : nuc6i7 running 64-bit Ubuntu 16.04.2 LTS
 //Command     : generate_target bd_be30_wrapper.bd
 //Design      : bd_be30_wrapper
