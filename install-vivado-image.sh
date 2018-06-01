7f4640a0a85d2a0f69143128e7a55921926510f4
zyx <zz593141477@gmail.com>
2018-06-01 20:24:25 +0800

    simplify script

--- install-vivado-image.sh
+++ install-vivado-image.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -e
 wget -c -O vivado-2018.1.tar 'https://cloud.tsinghua.edu.cn/f/4bbf722583ef4b1f87da/?dl=1'
-sudo docker import vivado-2018.1.tar vivado:import
-sudo docker run --name vivado-tmp vivado:import test
-sudo docker commit -c 'USER vivado' -c 'CMD ["sh","-c","exec /opt/Xilinx/Vivado/2018.1/bin/vivado"]' -c 'WORKDIR /home/vivado/project' vivado-tmp vivado:2018.1
-sudo docker rm vivado-tmp
+sudo docker import -c 'USER vivado' -c 'CMD ["sh","-c","exec /opt/Xilinx/Vivado/2018.1/bin/vivado"]' -c 'WORKDIR /home/vivado/project' vivado-2018.1.tar vivado:2018.1
 sudo docker images
 echo "The installation file can be safely removed now"
 rm -i vivado-2018.1.tar
