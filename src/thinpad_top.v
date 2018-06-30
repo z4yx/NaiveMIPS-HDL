622e21e0265b4c585ff63b5a6d507b6929e766db
zhang <zz593141477@gmail.com>
2018-06-30 11:12:24 +0800

    connecting wires

---
+++ src/thinpad_top.v
@@ -0,0 +1,137 @@
+`default_nettype none
+
+module thinpad_top(
+    input wire clk_50M,           //50MHz 时钟输入
+    input wire clk_11M0592,       //11.0592MHz 时钟输入
+
+    input wire clock_btn,         //BTN5手动时钟按钮开关，带消抖电路，按下时为1
+    input wire reset_btn,         //BTN6手动复位按钮开关，带消抖电路，按下时为1
+
+    input  wire[3:0]  touch_btn,  //BTN1~BTN4，按钮开关，按下时为1
+    input  wire[31:0] dip_sw,     //32位拨码开关，拨到“ON”时为1
+    output wire[15:0] leds,       //16位LED，输出时1点亮
+    output wire[7:0]  dpy0,       //数码管低位信号，包括小数点，输出1点亮
+    output wire[7:0]  dpy1,       //数码管高位信号，包括小数点，输出1点亮
+
+    //CPLD串口控制器信号
+    output wire uart_rdn,         //读串口信号，低有效
+    output wire uart_wrn,         //写串口信号，低有效
+    input wire uart_dataready,    //串口数据准备好
+    input wire uart_tbre,         //发送数据标志
+    input wire uart_tsre,         //数据发送完毕标志
+
+    //BaseRAM信号
+    inout wire[31:0] base_ram_data,  //BaseRAM数据，低8位与CPLD串口控制器共享
+    output wire[19:0] base_ram_addr, //BaseRAM地址
+    output wire[3:0] base_ram_be_n,  //BaseRAM字节使能，低有效。如果不使用字节使能，请保持为0
+    output wire base_ram_ce_n,       //BaseRAM片选，低有效
+    output wire base_ram_oe_n,       //BaseRAM读使能，低有效
+    output wire base_ram_we_n,       //BaseRAM写使能，低有效
+
+    //ExtRAM信号
+    inout wire[31:0] ext_ram_data,  //ExtRAM数据
+    output wire[19:0] ext_ram_addr, //ExtRAM地址
+    output wire[3:0] ext_ram_be_n,  //ExtRAM字节使能，低有效。如果不使用字节使能，请保持为0
+    output wire ext_ram_ce_n,       //ExtRAM片选，低有效
+    output wire ext_ram_oe_n,       //ExtRAM读使能，低有效
+    output wire ext_ram_we_n,       //ExtRAM写使能，低有效
+
+    //直连串口信号
+    output wire txd,  //直连串口发送端
+    input  wire rxd,  //直连串口接收端
+
+    //Flash存储器信号，参考 JS28F640 芯片手册
+    output wire [22:0]flash_a,      //Flash地址，a0仅在8bit模式有效，16bit模式无意义
+    inout  wire [15:0]flash_d,      //Flash数据
+    output wire flash_rp_n,         //Flash复位信号，低有效
+    output wire flash_vpen,         //Flash写保护信号，低电平时不能擦除、烧写
+    output wire flash_ce_n,         //Flash片选信号，低有效
+    output wire flash_oe_n,         //Flash读使能信号，低有效
+    output wire flash_we_n,         //Flash写使能信号，低有效
+    output wire flash_byte_n,       //Flash 8bit模式选择，低有效。在使用flash的16位模式时请设为1
+
+    //USB 控制器信号，参考 SL811 芯片手册
+    output wire sl811_a0,
+    inout  wire[7:0] sl811_d,
+    output wire sl811_wr_n,
+    output wire sl811_rd_n,
+    output wire sl811_cs_n,
+    output wire sl811_rst_n,
+    output wire sl811_dack_n,
+    input  wire sl811_intrq,
+    input  wire sl811_drq_n,
+
+    //网络控制器信号，参考 DM9000A 芯片手册
+    output wire dm9k_cmd,
+    inout  wire[15:0] dm9k_sd,
+    output wire dm9k_iow_n,
+    output wire dm9k_ior_n,
+    output wire dm9k_cs_n,
+    output wire dm9k_pwrst_n,
+    input  wire dm9k_int,
+
+    //图像输出信号
+    output wire[2:0] video_red,    //红色像素，3位
+    output wire[2:0] video_green,  //绿色像素，3位
+    output wire[1:0] video_blue,   //蓝色像素，2位
+    output wire video_hsync,       //行同步（水平同步）信号
+    output wire video_vsync,       //场同步（垂直同步）信号
+    output wire video_clk,         //像素时钟输出
+    output wire video_de           //行数据有效信号，用于区分消隐区
+);
+
+soc_toplevel soc(
+    .base_ram_data(base_ram_data),
+    .ext_ram_data(ext_ram_data),
+    .flash_data(flash_d),
+    .sl811_data(sl811_d),
+    .dm9k_data(dm9k_sd),
+    .gpio0({dpy1,dpy0,leds}),
+    .gpio1(dip_sw),
+    .base_ram_addr(base_ram_addr),
+    .base_ram_be(base_ram_be_n),
+    .base_ram_ce_n(base_ram_ce_n),
+    .base_ram_oe_n(base_ram_oe_n),
+    .base_ram_we_n(base_ram_we_n),
+    .ext_ram_addr(ext_ram_addr),
+    .ext_ram_be(ext_ram_be_n),
+    .ext_ram_ce_n(ext_ram_ce_n),
+    .ext_ram_oe_n(ext_ram_oe_n),
+    .ext_ram_we_n(ext_ram_we_n),
+    .txd(txd),
+    .flash_address(flash_a[22:1]),
+    .flash_rp_n(flash_rp_n),
+    .flash_vpen(flash_vpen),
+    .flash_oe_n(flash_oe_n),
+    .flash_ce(flash_ce_n),
+    .flash_byte_n(flash_byte_n),
+    .flash_we_n(flash_we_n),
+    .sl811_a0(sl811_a0),
+    .sl811_we_n(sl811_wr_n),
+    .sl811_rd_n(sl811_rd_n),
+    .sl811_cs_n(sl811_cs_n),
+    .sl811_rst_n(sl811_rst_n),
+    .sl811_drq(sl811_drq_n),
+    .dm9k_cmd(dm9k_cmd),
+    .dm9k_we_n(dm9k_iow_n),
+    .dm9k_rd_n(dm9k_ior_n),
+    .dm9k_cs_n(dm9k_cs_n),
+    .dm9k_rst_n(dm9k_pwrst_n),
+    .vga_pixel({video_red,video_green,video_blue}),
+    .vga_hsync(video_hsync),
+    .vga_vsync(video_vsync),
+    .vga_clk(video_clk),
+    .vga_de(video_de),
+    .uart_wrn(uart_wrn),
+    .uart_rdn(uart_rdn),
+    .clk_in(clk_50M),
+    .clk_uart_in(clk_11M0592),
+    .rxd(rxd),
+    .sl811_dack(sl811_dack_n),
+    .sl811_int(sl811_intrq),
+    .dm9k_int(dm9k_int),
+    .touch_btn({reset_btn,clock_btn,touch_btn})
+
+);
+
+endmodule
