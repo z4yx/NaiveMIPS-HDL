395d74918a1327d33d28247a7212c8127c0ef664
zyx <zyx@anywhere>
2018-09-29 00:18:53 +0800

    a generic method to launch ip runs

--- xilinx/NaiveMIPS/build.tcl
+++ xilinx/NaiveMIPS/build.tcl
@@ -1,12 +1,18 @@
 update_compile_order -fileset sources_1
 upgrade_ip [get_ips]
-create_ip_run [get_ips blk_mem_gen_v7_3]
-create_ip_run [get_ips clk_wiz_v3_6]
-create_ip_run [get_ips gpu_mem_xilinx]
-launch_runs -quiet -jobs 2 blk_mem_gen_v7_3_synth_1 clk_wiz_v3_6_synth_1 gpu_mem_xilinx_synth_1
-wait_on_run blk_mem_gen_v7_3_synth_1
-wait_on_run clk_wiz_v3_6_synth_1
-wait_on_run gpu_mem_xilinx_synth_1
+
+set ip_runs {}
+set run_suffix _synth_1
+foreach ip [get_ips] {
+    create_ip_run [get_ips $ip]
+    lappend ip_runs $ip$run_suffix
+}
+launch_runs -quiet -jobs 2 {*}$ip_runs
+
+foreach r $ip_runs {
+    wait_on_run $r
+}
+
 reset_run impl_1
 reset_run synth_1
 launch_runs -jobs 2 impl_1 -to_step write_bitstream
